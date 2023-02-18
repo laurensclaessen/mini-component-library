9e167bc6c8ee7c1215849db6b060bb4339f0ac95
Laurens Claessen <laurens.claessen@gmail.com>
2023-02-18 21:06:11 +0100

    make some progress

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,40 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  // return <progress id="loadinglabel" max="100" value={value}></progress>;
+
+  // return (
+  //   <span role="progressbar" aria-valuenow={value}>
+  //     <svg width="100" height="10">
+  //       <rect height="10" width="100" stroke="black" fill="black" />
+  //       <rect height="10" width={value} fill="grey" />
+  //     </svg>
+  //   </span>
+  // );
+
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value}>
+      <InnerBar style={{ "--value": `${value}%` }}></InnerBar>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  height: 12px;
+  overflow: hidden;
+  background: ${COLORS.transparentGray15};
+  border-radius: 4px;
+`;
+
+const InnerBar = styled.div`
+  height: 100%;
+  width: var(--value);
+  background: ${COLORS.primary};
+`;
 
 export default ProgressBar;
